2e80f7b8279ef522016da8b23419158c90e8c9d9
cthummel <thummelcorin@gmail.com>
2017-01-31 13:57:58 -0700

    minor changes

--- Spreadsheet/DependencyGraph/DependencyGraph.cs
+++ Spreadsheet/DependencyGraph/DependencyGraph.cs
@@ -128,7 +128,11 @@ namespace Dependencies
             }
             else
             {
-                GetDependees(s);
+                if(GetDependees(s) == null)
+                {
+
+                }
+                
             }
         }
 
